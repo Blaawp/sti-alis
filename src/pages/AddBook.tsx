82139f71bf71e34286c5262406bdc1f5d55fa605
BLAAP <134408767+Blaawp@users.noreply.github.com>
2023-12-15 10:29:26 +0800

    Update AddBook.tsx

--- src/pages/AddBook.tsx
+++ src/pages/AddBook.tsx
@@ -199,7 +199,7 @@ export default function AddBook() {
                 </div>
                 <div className="mb-4.5">
                   <label className="mb-2.5 block text-black dark:text-white">
-                    Ed <span className="text-meta-1">*</span>
+                    Edition <span className="text-meta-1">*</span>
                   </label>
                   <input
                     type="text"
@@ -235,7 +235,7 @@ export default function AddBook() {
                 </div>
                 <div className="mb-4.5">
                   <label className="mb-2.5 block text-black dark:text-white">
-                    ISBN <span className="text-meta-1">*</span>
+                    International Standard Book Number(ISBN) <span className="text-meta-1">*</span>
                   </label>
                   <input
                     type="text"
